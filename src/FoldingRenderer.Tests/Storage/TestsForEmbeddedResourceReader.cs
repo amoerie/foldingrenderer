ae1d14252b10feb07dfb42072ce88fdfe33176cd
Alexander Moerman <alexander.moerman@gmail.com>
2017-08-05 16:00:48 +0200

    Add reader to load embedded resources

---
+++ src/FoldingRenderer.Tests/Storage/TestsForEmbeddedResourceReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using FoldingRenderer.Storage;
+using Xunit;
+
+namespace FoldingRenderer.Tests.Storage
+{
+  public class TestsForEmbeddedResourceReader
+  {
+    private readonly EmbeddedResourceReader _sut;
+
+    public TestsForEmbeddedResourceReader()
+    {
+      _sut = new EmbeddedResourceReader();
+    }
+
+    public class Read : TestsForEmbeddedResourceReader
+    {
+      [Fact]
+      public void ShouldCorrectlyReadAllContents()
+      {
+        var contents = _sut.Read(typeof(TestsForEmbeddedResourceReader).Assembly,
+          typeof(TestsForEmbeddedResourceReader).Namespace + ".HelloWorld.txt");
+        contents.Should().NotBeNullOrEmpty().And.Be("Hello world!");
+      }
+
+      [Fact]
+      public void ShouldFailToReadIncorrectPath()
+      {
+        Action invalidPathRead = () => _sut.Read(typeof(TestsForEmbeddedResourceReader).Assembly,
+          typeof(TestsForEmbeddedResourceReader).Namespace + ".HelloWorld2.txt");
+        invalidPathRead.ShouldThrow<ArgumentException>();
+      }
+    }
+  }
+}
---
+++ src/FoldingRenderer/Storage/EmbeddedResourceReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace FoldingRenderer.Storage {
+  public interface IEmbeddedResourceReader {
+    string Read(Assembly assembly, string path);
+  }
+
+  public class EmbeddedResourceReader : IEmbeddedResourceReader {
+    public string Read(Assembly assembly, string path) {
+      using (var stream = assembly.GetManifestResourceStream(path)) {
+        if (stream == null) {
+          throw new ArgumentException("Embedded resource '" + path + "' could not be found.");
+        }
+        using (var reader = new StreamReader(stream)) {
+          return reader.ReadToEnd();
+        }
+      }
+    }
+  }
+}
