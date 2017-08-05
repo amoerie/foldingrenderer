2dbdbc57441c90f6c63e2261a931c6b86d4a6621
Alexander Moerman <alexander.moerman@gmail.com>
2017-08-05 11:29:31 +0200

    Add test project

---
+++ src/FoldingRenderer/Models/Panel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldingRenderer.Models {
+  public class Panel {
+    public Guid PanelId { get; set; }
+  }
+}
