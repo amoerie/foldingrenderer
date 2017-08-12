58d8c5390a631ee4e0b81300d8ea69587a114f84
Alexander Moerman <alexander.moerman@gmail.com>
2017-08-12 15:46:46 +0200

    Add rotator that can rotate rectangles. (in 4 directions only)

---
+++ src/FoldingRenderer.Tests/Drawing/TestsForRectangleRotator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using FluentAssertions;
+using FoldingRenderer.Domain.Drawing;
+using FoldingRenderer.Domain.Types;
+using Xunit;
+
+namespace FoldingRenderer.Tests.Domain.Drawing {
+  public class TestsForRectangleRotator {
+    readonly RectangleRotator _sut;
+
+    protected TestsForRectangleRotator() {
+      _sut = new RectangleRotator();
+    }
+
+    public class Rotate : TestsForRectangleRotator {
+      readonly Rectangle _rectangle;
+      readonly Position _hinge;
+
+      public Rotate() {
+        /*
+         *                             300px
+         *           <--------------------------------------->
+         *         (0,0)                                 (300,0) 
+         *       ^   o-------------------o-------------------o
+         *       |   |                                       |
+         *       |   |                                       |
+         * 100px |   |                                       |
+         *       |   |                                       |
+         *       |   |                                       |
+         *       v   o-------------------o-------------------o
+         *         (0,100)           (150,100)           (300,100)
+         *                          
+         */
+        _rectangle = new Rectangle(new Point(0,0), new Size(300,100));
+        _hinge = new Position().WithX(150).WithY(100);
+      }
+
+      [Fact]
+      public void ShouldJustReturnTheRectangleWhenRotationIsNone() {
+        _sut.Rotate(_hinge, _rectangle, Rotation.None).ShouldBeEquivalentTo(_rectangle);
+      }
+
+      [Theory]
+      [InlineData(-179)]
+      [InlineData(-1)]
+      [InlineData(1)]
+      [InlineData(179)]
+      public void ShouldThrowForUnsupportedRotations(int rotation) {
+        new Action(() => _sut.Rotate(_hinge, _rectangle, new Rotation(rotation))).ShouldThrow<NotSupportedException>();
+      }
+
+      [Fact]
+      public void ShouldCorrectlyRotateRight() {
+        var actualRectangle = _sut.Rotate(_hinge, _rectangle, Rotation.Right);
+        var expectedRectangle = new Rectangle(new Point(150,-50), new Size(100,300));
+        actualRectangle.ShouldBeEquivalentTo(expectedRectangle);
+      }
+
+      [Fact]
+      public void ShouldCorrectlyRotateLeft() {
+        var actualRectangle = _sut.Rotate(_hinge, _rectangle, Rotation.Left);
+        var expectedRectangle = new Rectangle(new Point(50,-50), new Size(100, 300));
+        actualRectangle.ShouldBeEquivalentTo(expectedRectangle);
+      }
+
+      [Fact]
+      public void ShouldCorrectlyRotateDown() {
+        var actualRectangle = _sut.Rotate(_hinge, _rectangle, Rotation.Down);
+        var expectedRectangle = new Rectangle(new Point(0,100), new Size(300, 100));
+        actualRectangle.ShouldBeEquivalentTo(expectedRectangle);
+      }
+
+    }
+  }
+}
---
+++ src/FoldingRenderer/Drawing/RectangleRotator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using FoldingRenderer.Domain.Types;
+
+namespace FoldingRenderer.Domain.Drawing {
+  public interface IRectangleRotator {
+    Rectangle Rotate(Position hinge, Rectangle rectangle, Rotation rotation);
+  }
+
+  public class RectangleRotator : IRectangleRotator {
+    public Rectangle Rotate(Position hinge, Rectangle rectangle, Rotation rotation) {
+      if (Rotation.None.Equals(rotation)) return rectangle;
+      if (Rotation.Right.Equals(rotation)) {
+        var topLeft = new Point(hinge.X, hinge.Y - rectangle.Width / 2);
+        return new Rectangle(topLeft, new Size(rectangle.Size.Height, rectangle.Size.Width));
+      }
+      if (Rotation.Left.Equals(rotation)) {
+        var topLeft = new Point(hinge.X - rectangle.Height, hinge.Y - rectangle.Width / 2);
+        return new Rectangle(topLeft, new Size(rectangle.Size.Height, rectangle.Size.Width));
+      }
+      if (Rotation.Down.Equals(rotation)) {
+        var topLeft = new Point(hinge.X - rectangle.Width / 2, hinge.Y);
+        return new Rectangle(topLeft, new Size(rectangle.Size.Width, rectangle.Size.Height));
+      }
+      throw new NotSupportedException($"Rotations other than None, Right, Left or Down are not supported. Attempted to rotate by {rotation.Value}°");
+    }
+  }
+}
