67bef0a751eb20d0348bdfeab32aa0d5fd0f9530
Alexander Moerman <alexander.moerman@gmail.com>
2017-08-12 14:07:40 +0200

    Add abstraction for Image/Bitmap called ICanvas, which is a holder object for the Bitmap we're creating. This considerably facilitates unit testing

---
+++ src/FoldingRenderer/Domain/Drawing/Canvas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace FoldingRenderer.Domain.Drawing {
+  public interface ICanvas {
+    Bitmap Bitmap { get; }
+  }
+
+  public class Canvas : ICanvas {
+    public Canvas(Bitmap bitmap) {
+      if (bitmap == null) throw new ArgumentNullException(nameof(bitmap));
+      Bitmap = bitmap;
+    }
+
+    public Bitmap Bitmap { get; }
+  }
+}
