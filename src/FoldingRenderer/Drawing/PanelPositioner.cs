af2b982251b5a13d0ea3720184a1d091f4322344
Alexander Moerman <alexander.moerman@gmail.com>
2017-08-12 15:47:04 +0200

    Remove domain folder.. just have top level folders for 'Drawing' and 'Types'

--- src/FoldingRenderer/Drawing/PanelPositioner.cs
+++ src/FoldingRenderer/Drawing/PanelPositioner.cs
@@ -10,28 +10,30 @@ namespace FoldingRenderer.Domain.Drawing {
     readonly IAttachToHingeDeterminer _attachToHingeDeterminer;
     readonly IRotationDeterminer _rotationDeterminer;
     readonly IRectangleFactory _rectangleFactory;
+    readonly IRectangleRotator _rectangleRotator;
 
     public PanelPositioner(IAttachToHingeDeterminer attachToHingeDeterminer,
       IRotationDeterminer rotationDeterminer,
-      IRectangleFactory rectangleFactory) {
+      IRectangleFactory rectangleFactory,
+      IRectangleRotator rectangleRotator) {
       if (attachToHingeDeterminer == null) throw new ArgumentNullException(nameof(attachToHingeDeterminer));
       if (rotationDeterminer == null) throw new ArgumentNullException(nameof(rotationDeterminer));
       if (rectangleFactory == null) throw new ArgumentNullException(nameof(rectangleFactory));
+      if (rectangleRotator == null) throw new ArgumentNullException(nameof(rectangleRotator));
       _attachToHingeDeterminer = attachToHingeDeterminer;
       _rotationDeterminer = rotationDeterminer;
       _rectangleFactory = rectangleFactory;
+      _rectangleRotator = rectangleRotator;
     }
 
     public PanelRectangle Position(PanelRectangle parent, Panel panel) {
       if (parent == null) throw new ArgumentNullException(nameof(parent));
       if (panel == null) throw new ArgumentNullException(nameof(panel));
       var hingeToAttachTo = _attachToHingeDeterminer.Determine(parent, panel);
-      var rotation = _rotationDeterminer.Determine(parent, panel);
       var rectangle = _rectangleFactory.Create(hingeToAttachTo, panel.Dimensions);
-
-      //var panelRectangle = new PanelRectangle(panel, rectangle);
-
-      throw new NotImplementedException();
+      var rotation = _rotationDeterminer.Determine(parent, panel);
+      var rotatedRectangle = _rectangleRotator.Rotate(hingeToAttachTo, rectangle, rotation);
+      return new PanelRectangle(panel, rotatedRectangle, rotation);
     }
   }
 }
