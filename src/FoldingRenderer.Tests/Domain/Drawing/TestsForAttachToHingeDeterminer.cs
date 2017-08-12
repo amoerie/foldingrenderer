dbae56a5a05f1ee0f9604c06df205aa195dd80e2
Alexander Moerman <alexander.moerman@gmail.com>
2017-08-12 14:02:05 +0200

    Add determiner which knows which hinge to attach to based on the parent and the panel.AttachToSide parameter

---
+++ src/FoldingRenderer.Tests/Domain/Drawing/TestsForAttachToHingeDeterminer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using FluentAssertions;
+using FoldingRenderer.Domain.Drawing;
+using FoldingRenderer.Domain.Types;
+using Xunit;
+
+namespace FoldingRenderer.Tests.Domain.Drawing {
+  public class TestsForAttachToHingeDeterminer {
+    readonly AttachToHingeDeterminer _sut;
+
+    protected TestsForAttachToHingeDeterminer() {
+      _sut = new AttachToHingeDeterminer();
+    }
+
+    public class Determine : TestsForAttachToHingeDeterminer {
+      readonly PanelRectangle _parent;
+      readonly Panel _panel;
+
+      public Determine() {
+        _panel = new Panel();
+        _parent = new PanelRectangle(_panel, new Rectangle(0,0,100,100), Rotation.None);
+      }
+
+      [Fact]
+      public void ShouldThrowForNullArguments() {
+        new Action(() => _sut.Determine(null, _panel)).ShouldThrow<ArgumentNullException>();
+        new Action(() => _sut.Determine(_parent, null)).ShouldThrow<ArgumentNullException>();
+      }
+
+      [Fact]
+      public void ShouldReturnBottomHingeWhenSideIsZero() {
+        var hinge = _sut.Determine(_parent, _panel.WithAttachedToSide(0));
+        hinge.Should().Be(_parent.Hinges.Bottom);
+      }
+
+      [Fact]
+      public void ShouldReturnRightHingeWhenSideIsOne() {
+        var hinge = _sut.Determine(_parent, _panel.WithAttachedToSide(1));
+        hinge.Should().Be(_parent.Hinges.Right);
+      }
+
+      [Fact]
+      public void ShouldReturnTopHingeWhenSideIsTwo() {
+        var hinge = _sut.Determine(_parent, _panel.WithAttachedToSide(2));
+        hinge.Should().Be(_parent.Hinges.Top);
+      }
+
+      [Fact]
+      public void ShouldReturnLeftHingeWhenSideIsThree() {
+        var hinge = _sut.Determine(_parent, _panel.WithAttachedToSide(3));
+        hinge.Should().Be(_parent.Hinges.Left);
+      }
+
+      [Theory]
+      [InlineData(-1)]
+      [InlineData(4)]
+      [InlineData(500)]
+      public void ShouldThrowArgumentOutOfRangeExceptionWhenSideIsOutsideValidRange(int side) {
+        new Action(() => _sut.Determine(_parent, _panel.WithAttachedToSide(side))).ShouldThrow<ArgumentOutOfRangeException>();
+      }
+
+
+    }
+  }
+}
---
+++ src/FoldingRenderer/Domain/Drawing/AttachToHingeDeterminer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FoldingRenderer.Domain.Types;
+
+namespace FoldingRenderer.Domain.Drawing {
+  public interface IAttachToHingeDeterminer {
+    /// <summary>
+    /// Determines the hinge of the <paramref name="parent"/> to which the <paramref name="child"/> panel should attach.
+    /// </summary>
+    Position Determine(PanelRectangle parent, Panel child);
+  }
+
+  public class AttachToHingeDeterminer : IAttachToHingeDeterminer {
+    public Position Determine(PanelRectangle parent, Panel panel) {
+      if (parent == null) throw new ArgumentNullException(nameof(parent));
+      if (panel == null) throw new ArgumentNullException(nameof(panel));
+
+      switch (panel.AttachedToSide) {
+        case 0: return parent.Hinges.Bottom;
+        case 1: return parent.Hinges.Right;
+        case 2: return parent.Hinges.Top;
+        case 3: return parent.Hinges.Left;
+        default:
+          throw new ArgumentOutOfRangeException($"Panel '{panel.Name}' ({panel.Id}) does not have a valid side to attach to: {panel.AttachedToSide}");
+      }
+    }
+  }
+}
