c00e126e7f80cc9faf23a5a4ab00fb5cd38053d0
Alexander Moerman <alexander.moerman@gmail.com>
2017-08-12 14:00:12 +0200

    Add PanelHinges, a class which takes a rectangle and a rotation and that determines the bottom, left, top and right hinge of the panel

---
+++ src/FoldingRenderer.Tests/Domain/Drawing/TestsForPanelHinges.cs
@@ -0,0 +1,157 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using FoldingRenderer.Domain.Drawing;
+using FoldingRenderer.Domain.Types;
+using Xunit;
+
+namespace FoldingRenderer.Tests.Domain.Drawing {
+  public class TestsForPanelHinges {
+    public class BottomHinge : TestsForPanelHinges {
+      [Fact]
+      public void ShouldReturnCorrectPositionForUnevenWidth() {
+        // Top left = (0,0)
+        // Dimensions = 15x15
+        var rectangle = new Rectangle(0, 0, 15, 15);
+
+        var expectedBottomHinge = new Position().WithX(7).WithY(15);
+        var actualBottomHinge = new PanelHinges(rectangle, Rotation.None).Bottom;
+
+        actualBottomHinge.ShouldBeEquivalentTo(expectedBottomHinge);
+      }
+
+      [Fact]
+      public void ShouldReturnCorrectPositionForEvenWidth() {
+        // Top left = (0,0)
+        // Dimensions = 150x150
+        var rectangle = new Rectangle(0, 0, 150, 150);
+
+        var expectedBottomHinge = new Position().WithX(75).WithY(150);
+        var actualBottomHinge = new PanelHinges(rectangle, Rotation.None).Bottom;
+
+        actualBottomHinge.ShouldBeEquivalentTo(expectedBottomHinge);
+      }
+
+      [Fact]
+      public void ShouldReturnCorrectHingeForEveryRotation() {
+        // Top left = (0,0)
+        // Dimensions = 15x15
+        var rectangle = new Rectangle(0, 0, 15, 15);
+        var noRotation = new PanelHinges(rectangle, Rotation.None);
+        new PanelHinges(rectangle, Rotation.Right).Bottom.Should().Be(noRotation.Left);
+        new PanelHinges(rectangle, Rotation.Left).Bottom.Should().Be(noRotation.Right);
+        new PanelHinges(rectangle, Rotation.Down).Bottom.Should().Be(noRotation.Top);
+      }
+    }
+
+    public class TopHinge : TestsForPanelHinges {
+      [Fact]
+      public void ShouldReturnCorrectPositionForUnevenWidth() {
+        // Top left = (0,0)
+        // Dimensions = 15x15
+        var rectangle = new Rectangle(0, 0, 15, 15);
+
+        var expectedTopHinge = new Position().WithX(7).WithY(0);
+        var actualTopHinge = new PanelHinges(rectangle, Rotation.None).Top;
+
+        actualTopHinge.ShouldBeEquivalentTo(expectedTopHinge);
+      }
+
+      [Fact]
+      public void ShouldReturnCorrectPositionForEvenWidth() {
+        // Top left = (0,0)
+        // Dimensions = 150x150
+        var rectangle = new Rectangle(0, 0, 150, 150);
+
+        var expectedTopHinge = new Position().WithX(75).WithY(0);
+        var actualTopHinge = new PanelHinges(rectangle, Rotation.None).Top;
+
+        actualTopHinge.ShouldBeEquivalentTo(expectedTopHinge);
+      }
+
+      [Fact]
+      public void ShouldReturnCorrectHingeForEveryRotation() {
+        // Top left = (0,0)
+        // Dimensions = 15x15
+        var rectangle = new Rectangle(0, 0, 15, 15);
+        var noRotation = new PanelHinges(rectangle, Rotation.None);
+        new PanelHinges(rectangle, Rotation.Right).Top.Should().Be(noRotation.Right);
+        new PanelHinges(rectangle, Rotation.Left).Top.Should().Be(noRotation.Left);
+        new PanelHinges(rectangle, Rotation.Down).Top.Should().Be(noRotation.Bottom);
+      }
+    }
+
+    public class LeftHinge : TestsForPanelHinges {
+      [Fact]
+      public void ShouldReturnCorrectPositionForUnevenHeight() {
+        // Top left = (0,0)
+        // Dimensions = 15x15
+        var rectangle = new Rectangle(0, 0, 15, 15);
+
+        var expectedLeftHinge = new Position().WithX(0).WithY(7);
+        var actualLeftHinge = new PanelHinges(rectangle, Rotation.None).Left;
+
+        actualLeftHinge.ShouldBeEquivalentTo(expectedLeftHinge);
+      }
+
+      [Fact]
+      public void ShouldReturnCorrectPositionForEvenHeight() {
+        // Top left = (0,0)
+        // Dimensions = 150x150
+        var rectangle = new Rectangle(0, 0, 150, 150);
+
+        var expectedLeftHinge = new Position().WithX(0).WithY(75);
+        var actualLeftHinge = new PanelHinges(rectangle, Rotation.None).Left;
+
+        actualLeftHinge.ShouldBeEquivalentTo(expectedLeftHinge);
+      }
+
+      [Fact]
+      public void ShouldReturnCorrectHingeForEveryRotation() {
+        // Top left = (0,0)
+        // Dimensions = 15x15
+        var rectangle = new Rectangle(0, 0, 15, 15);
+        var noRotation = new PanelHinges(rectangle, Rotation.None);
+        new PanelHinges(rectangle, Rotation.Right).Left.Should().Be(noRotation.Top);
+        new PanelHinges(rectangle, Rotation.Left).Left.Should().Be(noRotation.Bottom);
+        new PanelHinges(rectangle, Rotation.Down).Left.Should().Be(noRotation.Right);
+      }
+    }
+
+    public class RightHinge : TestsForPanelHinges {
+      [Fact]
+      public void ShouldReturnCorrectPositionForUnevenHeight() {
+        // Top left = (0,0)
+        // Dimensions = 15x15
+        var rectangle = new Rectangle(0, 0, 15, 15);
+
+        var expectedRightHinge = new Position().WithX(15).WithY(7);
+        var actualRightHinge = new PanelHinges(rectangle, Rotation.None).Right;
+
+        actualRightHinge.ShouldBeEquivalentTo(expectedRightHinge);
+      }
+
+      [Fact]
+      public void ShouldReturnCorrectPositionForEvenHeight() {
+        // Top left = (0,0)
+        // Dimensions = 150x150
+        var rectangle = new Rectangle(0, 0, 150, 150);
+
+        var expectedRightHinge = new Position().WithX(150).WithY(75);
+        var actualRightHinge = new PanelHinges(rectangle, Rotation.None).Right;
+
+        actualRightHinge.ShouldBeEquivalentTo(expectedRightHinge);
+      }
+
+      [Fact]
+      public void ShouldReturnCorrectHingeForEveryRotation() {
+        // Top left = (0,0)
+        // Dimensions = 15x15
+        var rectangle = new Rectangle(0, 0, 15, 15);
+        var noRotation = new PanelHinges(rectangle, Rotation.None);
+        new PanelHinges(rectangle, Rotation.Right).Right.Should().Be(noRotation.Bottom);
+        new PanelHinges(rectangle, Rotation.Left).Right.Should().Be(noRotation.Top);
+        new PanelHinges(rectangle, Rotation.Down).Right.Should().Be(noRotation.Left);
+      }
+    }
+  }
+}
---
+++ src/FoldingRenderer/Domain/Drawing/PanelHinges.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using FoldingRenderer.Domain.Types;
+
+
+namespace FoldingRenderer.Domain.Drawing {
+  public class PanelHinges {
+    public Position Top { get; }
+    public Position Right { get; }
+    public Position Bottom { get; }
+    public Position Left { get; }
+
+    public PanelHinges(Rectangle rectangle, Rotation rotation) {
+      var bottomPosition = new Position().WithX(rectangle.X + rectangle.Width / 2).WithY(rectangle.Y + rectangle.Height);
+      var topPosition = new Position().WithX(rectangle.X + rectangle.Width / 2).WithY(rectangle.Y);
+      var leftPosition = new Position().WithX(rectangle.X).WithY(rectangle.Y + rectangle.Height / 2);
+      var rightPosition = new Position().WithX(rectangle.X + rectangle.Width).WithY(rectangle.Y + rectangle.Height / 2);
+
+      if (Rotation.None.Equals(rotation)) {
+        Bottom = bottomPosition;
+        Top = topPosition;
+        Left = leftPosition;
+        Right = rightPosition;
+      } else if (Rotation.Right.Equals(rotation)) {
+        Bottom = leftPosition;
+        Top = rightPosition;
+        Left = topPosition;
+        Right = bottomPosition;
+      } else if (Rotation.Left.Equals(rotation)) {
+        Bottom = rightPosition;
+        Top = leftPosition;
+        Left = bottomPosition;
+        Right = topPosition;
+      } else if (Rotation.Down.Equals(rotation)) {
+        Bottom = topPosition;
+        Top = bottomPosition;
+        Left = rightPosition;
+        Right = leftPosition;
+      }
+    }
+  }
+}
