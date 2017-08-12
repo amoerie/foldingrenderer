51ef04954b220909c8693f619264086722b87559
Alexander Moerman <alexander.moerman@gmail.com>
2017-08-12 14:05:12 +0200

    Add class which can determine the rotation of a panel based on the parent rotation and the AttachToSide property of a panel

---
+++ src/FoldingRenderer.Tests/Domain/Drawing/TestsForRotationDeterminer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using FluentAssertions;
+using FoldingRenderer.Domain.Drawing;
+using FoldingRenderer.Domain.Types;
+using Xunit;
+
+namespace FoldingRenderer.Tests.Domain.Drawing {
+  public class TestsForRotationDeterminer {
+    readonly RotationDeterminer _sut;
+
+    protected TestsForRotationDeterminer() {
+      _sut = new RotationDeterminer();
+    }
+
+    public class Determine : TestsForRotationDeterminer {
+      readonly PanelRectangle _parent;
+      readonly Panel _panel;
+
+      public Determine() {
+        _panel = new Panel();
+        _parent = new PanelRectangle(_panel, new Rectangle(0,0,100,100), Rotation.None);
+      }
+
+      [Fact]
+      public void ShouldThrowForNullArguments() {
+        new Action(() => _sut.Determine(null, _panel)).ShouldThrow<ArgumentNullException>();
+        new Action(() => _sut.Determine(_parent, null)).ShouldThrow<ArgumentNullException>();
+      }
+
+      [Fact]
+      public void ShouldReturnDownWhenSideIsZero() {
+        var hinge = _sut.Determine(_parent, _panel.WithAttachedToSide(0));
+        hinge.Should().Be(Rotation.Down);
+      }
+
+      [Fact]
+      public void ShouldReturnRightWhenSideIsOne() {
+        var hinge = _sut.Determine(_parent, _panel.WithAttachedToSide(1));
+        hinge.Should().Be(Rotation.Right);
+      }
+
+      [Fact]
+      public void ShouldReturnNoneWhenSideIsTwo() {
+        var hinge = _sut.Determine(_parent, _panel.WithAttachedToSide(2));
+        hinge.Should().Be(Rotation.None);
+      }
+
+      [Fact]
+      public void ShouldReturnLeftWhenSideIsThree() {
+        var hinge = _sut.Determine(_parent, _panel.WithAttachedToSide(3));
+        hinge.Should().Be(Rotation.Left);
+      }
+
+      [Theory]
+      [InlineData(-1)]
+      [InlineData(4)]
+      [InlineData(500)]
+      public void ShouldThrowArgumentOutOfRangeExceptionWhenSideIsOutsideValidRange(int side) {
+        new Action(() => _sut.Determine(_parent, _panel.WithAttachedToSide(side))).ShouldThrow<ArgumentOutOfRangeException>();
+      }
+
+
+    }
+  }
+}
---
+++ src/FoldingRenderer/Domain/Drawing/RotationDeterminer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FoldingRenderer.Domain.Types;
+
+namespace FoldingRenderer.Domain.Drawing {
+  public interface IRotationDeterminer {
+    /// <summary>
+    /// Determines the rotation of the panel relative to its parent.
+    /// </summary>
+    Rotation Determine(PanelRectangle parent, Panel child);
+  }
+
+  public class RotationDeterminer : IRotationDeterminer {
+    public Rotation Determine(PanelRectangle parent, Panel panel) {
+      if (parent == null) throw new ArgumentNullException(nameof(parent));
+      if (panel == null) throw new ArgumentNullException(nameof(panel));
+
+      switch (panel.AttachedToSide) {
+        case 0: return parent.Rotation + Rotation.Down;
+        case 1: return parent.Rotation + Rotation.Right;
+        case 2: return parent.Rotation + Rotation.None;
+        case 3: return parent.Rotation + Rotation.Left;
+        default:
+          throw new ArgumentOutOfRangeException($"Panel '{panel.Name}' ({panel.Id}) does not have a valid side to attach to: {panel.AttachedToSide}");
+      }
+    }
+  }
+}
